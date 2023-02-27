58b424c5786779848441ab3f70b082d46b664496
scorpio1147 <danesh.siva12@gmail.com>
2023-02-27 12:41:24 -0500

    added disc 4 solution

--- discussions/d4_hof/d4_skeleton.ml
+++ discussions/d4_hof/d4_skeleton.ml
@@ -17,10 +17,19 @@ let rec fold_right f lst a =
     []->a
     |h::t-> f h (fold_right f t a)
 
-let rec map_tree f t = failwith "unimplemented"
+let rec map_tree f t = match t with
+| Leaf -> Leaf
+| Node (left_tree, value, right_tree) -> 
+  Node (map_tree f left_tree, f value, map_tree f right_tree)
 
-let rec fold_tree f b t = failwith "unimplemented"
+let rec fold_tree f b t = match t with
+| Leaf -> b
+| Node(l, v, r) -> let res_l = fold_tree f b l in
+let res_r = fold_tree f b r in f res_l v res_r
 
 let add1 tree = failwith "unimplemented"
 
-let sum tree = failwith "unimplemented"
+let sum tree = fold_tree (fun x l r -> x + l + r) 0 tree
+
+
+Node of a tree * a * a tree
